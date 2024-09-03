9a2b918b4db738f3755d7e3e87daef6a3266f15b
Janno Kusman <janno.kusman@cyber.ee>
2024-09-03 11:07:43 +0300

    Add use_latest_release.sh script to update cdoc2 dependencies

---
+++ use_latest_release.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# replace ee.cyber.cdoc2:* dependency versions with latest release version (includes packages from local maven repo)
+mvn versions:use-latest-versions -Dincludes=ee.cyber.cdoc2:* -DexcludeReactor=false -DallowSnapshots=false
+
+# put and get server have spring-boot as parent and need to be updated separately
+
+mvn -f put-server versions:use-latest-versions -Dincludes=ee.cyber.cdoc2:* -DexcludeReactor=false -DallowSnapshots=false
+
+mvn -f get-server versions:use-latest-versions -Dincludes=ee.cyber.cdoc2:* -DexcludeReactor=false -DallowSnapshots=false
