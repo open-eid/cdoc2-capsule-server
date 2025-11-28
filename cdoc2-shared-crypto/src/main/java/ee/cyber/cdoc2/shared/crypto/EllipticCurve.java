6057eabb521be0fa4e3b31b32b2020daa664e1a9
Hain Zuppur <hain.zuppur@cyber.ee>
2025-11-28 10:49:07 +0200

    CDOC-42: Remove dependency to cdoc2-lib and remove unused methods from EllipticCurve

--- cdoc2-shared-crypto/src/main/java/ee/cyber/cdoc2/shared/crypto/EllipticCurve.java
+++ cdoc2-shared-crypto/src/main/java/ee/cyber/cdoc2/shared/crypto/EllipticCurve.java
@@ -1,68 +1,32 @@
 package ee.cyber.cdoc2.shared.crypto;
 
-import java.nio.ByteBuffer;
-import java.security.GeneralSecurityException;
-import java.security.InvalidKeyException;
-import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.PublicKey;
-import java.security.interfaces.ECPublicKey;
-import java.security.spec.InvalidParameterSpecException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 /**
- * Curve values from {@link ee.cyber.cdoc2.fbs.recipients.EllipticCurve} defined as enum and mapped to
- * known elliptic curve names and oid's
+ * Elliptic curve enums mapped to known elliptic curve names and oid's
  */
 public enum EllipticCurve {
 
-    UNKNOWN(ee.cyber.cdoc2.fbs.recipients.EllipticCurve.UNKNOWN, null, null),
-    SECP384R1(ee.cyber.cdoc2.fbs.recipients.EllipticCurve.secp384r1, ECKeys.SECP_384_R_1, ECKeys.SECP_384_OID),
-    SECP256R1(ee.cyber.cdoc2.fbs.recipients.EllipticCurve.secp256r1, ECKeys.SECP_256_R_1, ECKeys.SECP_256_OID);
+    UNKNOWN(null, null),
+    SECP384R1(ECKeys.SECP_384_R_1, ECKeys.SECP_384_OID),
+    SECP256R1(ECKeys.SECP_256_R_1, ECKeys.SECP_256_OID);
 
     private static final Logger log = LoggerFactory.getLogger(EllipticCurve.class);
 
-    private final byte value;
     private final String name;
     private final String oid;
 
-    EllipticCurve(byte value, String name, String oid) {
-        this.value = value;
+    EllipticCurve(String name, String oid) {
         this.name = name;
         this.oid = oid;
     }
 
-    public byte getValue() {
-        return value;
-    }
-
     public String getName() {
         return name;
-    }
-    public String getOid() {
-        return oid;
-    }
-
-    public boolean isValidKey(ECPublicKey key) throws GeneralSecurityException {
-        return switch (this) {
-            case SECP384R1 -> ECKeys.isValidSecP384R1(key);
-            case SECP256R1 -> ECKeys.isEcSecp256r1Curve(key);
-            default -> throw new IllegalStateException("isValidKey not implemented for " + this);
-        };
-    }
-
-    public boolean isValidKeyPair(KeyPair keyPair) throws GeneralSecurityException {
-        if (this == EllipticCurve.SECP384R1) {
-            return ECKeys.isECSecp384r1(keyPair);
-        }
-        if (this == EllipticCurve.SECP256R1) {
-            return ECKeys.isECSecp256r1(keyPair);
-        }
-        throw new IllegalStateException("isValidKeyPair not implemented for " + this);
     }
 
     /**
@@ -76,80 +40,12 @@ public enum EllipticCurve {
         };
     }
 
-    public ECPublicKey decodeFromTls(ByteBuffer encoded) throws GeneralSecurityException {
-        return switch (this) {
-            case SECP384R1 -> ECKeys.decodeSecP384R1EcPublicKeyFromTls(encoded);
-            case SECP256R1 -> ECKeys.decodeSecP256R1EcPublicKeyFromTls(encoded);
-            default -> throw new IllegalStateException("decodeFromTls not implemented for " + this);
-        };
-    }
-
-    public KeyPair generateEcKeyPair() throws GeneralSecurityException {
-        return ECKeys.generateEcKeyPair(this.getName());
-    }
-
-    public static EllipticCurve forName(String name) throws NoSuchAlgorithmException {
-        if (ECKeys.SECP_384_R_1.equalsIgnoreCase(name)) {
-            return SECP384R1;
-        }
-        throw new NoSuchAlgorithmException("Unknown curve name " + name);
-    }
-
     public static EllipticCurve forOid(String oid) throws NoSuchAlgorithmException {
         return switch (oid) {
             case ECKeys.SECP_384_OID -> SECP384R1;
             case ECKeys.SECP_256_OID -> SECP256R1;
             default -> throw new NoSuchAlgorithmException("Unknown EC curve oid " + oid);
         };
-    }
-
-    public static EllipticCurve forValue(byte value) throws NoSuchAlgorithmException {
-        return switch (value) {
-            case ee.cyber.cdoc2.fbs.recipients.EllipticCurve.secp384r1 -> SECP384R1;
-            case ee.cyber.cdoc2.fbs.recipients.EllipticCurve.secp256r1 -> SECP256R1;
-            default -> throw new NoSuchAlgorithmException("Unknown EC curve value " + value);
-        };
-    }
-
-    /**
-     * @param publicKey ECPublicKey
-     * @return EllipticCurve
-     * @throws NoSuchAlgorithmException      if publicKey EC curve is not supported
-     * @throws InvalidParameterSpecException
-     * @throws NoSuchProviderException
-     * @throws InvalidKeyException           if publicKey is not ECPublicKey
-     */
-    public static EllipticCurve forPubKey(PublicKey publicKey) throws NoSuchAlgorithmException,
-        InvalidParameterSpecException, NoSuchProviderException, InvalidKeyException {
-
-        if (publicKey instanceof ECPublicKey ecPublicKey) {
-            return forOid(ECKeys.getCurveOid(ecPublicKey));
-        } else {
-            throw new InvalidKeyException("Unsupported key algorithm " + publicKey.getAlgorithm());
-        }
-    }
-
-    /**
-     * Check if public key is supported by CDOC lib
-     *
-     * @param publicKey to check for encryption by CDOC
-     * @return if publicKey is supported for encryption by CDOC
-     */
-    public static boolean isSupported(PublicKey publicKey) {
-        try {
-            EllipticCurve curve = forPubKey(publicKey);
-            return curve.isValidKey((ECPublicKey) publicKey);
-        } catch (GeneralSecurityException ge) {
-            log.info("Unsupported public key {}", ge.toString());
-            return false;
-        }
-    }
-
-    /**
-     * Supported curve names
-     */
-    public static String[] names() {
-        return ee.cyber.cdoc2.fbs.recipients.EllipticCurve.names;
     }
 
 }
